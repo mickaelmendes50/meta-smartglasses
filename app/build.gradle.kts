4a5e641b822ee6ce897af22af7ae7034cbf3c596
Mickael Mesquita <mesquita@aospa.co>
2026-05-29 09:48:19 -0300

    chore: Reformat Gradle classes
    Signed-off-by: Mickael Mesquita <mesquita@aospa.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,73 +7,76 @@
  */
 
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.jetbrains.kotlin.android)
-  alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
-  namespace = "br.ufg.akcit.smartglasses"
-  compileSdk = 35
+    namespace = "br.ufg.akcit.smartglasses"
+    compileSdk = 35
 
-  buildFeatures { buildConfig = true }
+    buildFeatures { buildConfig = true }
 
-  defaultConfig {
-    applicationId = "br.ufg.akcit.smartglasses"
-    minSdk = 31
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    defaultConfig {
+        applicationId = "br.ufg.akcit.smartglasses"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    vectorDrawables { useSupportLibrary = true }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables { useSupportLibrary = true }
 
-    // Meta Wearables Device Access Toolkit Setup
-    // Without Developer Mode, these values need to be set with credentials from the app registered
-    // in Wearables Developer Center
-    manifestPlaceholders["mwdat_application_id"] = ""
-    manifestPlaceholders["mwdat_client_token"] = ""
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = true
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("debug")
+        // Meta Wearables Device Access Toolkit Setup
+        // Without Developer Mode, these values need to be set with credentials from the app registered
+        // in Wearables Developer Center
+        manifestPlaceholders["mwdat_application_id"] = ""
+        manifestPlaceholders["mwdat_client_token"] = ""
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions { jvmTarget = "1.8" }
-  buildFeatures { compose = true }
-  composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
-  packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-  signingConfigs {
-    getByName("debug") {
-      storeFile = file("sample.keystore")
-      storePassword = "sample"
-      keyAlias = "sample"
-      keyPassword = "sample"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
-  }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions { jvmTarget = "1.8" }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("sample.keystore")
+            storePassword = "sample"
+            keyAlias = "sample"
+            keyPassword = "sample"
+        }
+    }
 }
 
 dependencies {
-  implementation(libs.androidx.activity.compose)
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(libs.androidx.exifinterface)
-  implementation(libs.androidx.lifecycle.runtime.compose)
-  implementation(libs.androidx.lifecycle.viewmodel.compose)
-  implementation(libs.androidx.material.icons.extended)
-  implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.exifinterface)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.collections.immutable)
-  implementation(libs.mwdat.core)
-  implementation(libs.mwdat.camera)
-  implementation(libs.mwdat.mockdevice)
-  androidTestImplementation(libs.androidx.ui.test.junit4)
-  androidTestImplementation(libs.androidx.test.uiautomator)
-  androidTestImplementation(libs.androidx.test.rules)
+    implementation(libs.mwdat.core)
+    implementation(libs.mwdat.camera)
+    implementation(libs.mwdat.mockdevice)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.uiautomator)
+    androidTestImplementation(libs.androidx.test.rules)
 }
